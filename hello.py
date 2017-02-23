2dc728d1e6399d4b74cf850f9a92fca51e97ae33
TorzsaCristian <TorzsaCristian@users.noreply.github.com>
2017-02-23 21:58:42 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb::/root:root@localhost/myappdatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb:/root:root@localhost/myappdatabase'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
