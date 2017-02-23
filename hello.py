d88f7ea0c38994fdccd055cc7600ddc417803283
TorzsaCristian <TorzsaCristian@users.noreply.github.com>
2017-02-23 21:42:52 +0000

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb::/root:root@localhost/myappdatabase'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key = True)
+    data = db.Column(db.String(255))
+
+    def __init__(self, data):
+        self.id = 0
+        self.data = data
+
+
+@app.route('/')
+def index():
+    db.session.add(Comment("WTFFFFFFF"))
+    com = Comment.query.all()
+    return jsonify({'response': com[0].data})
